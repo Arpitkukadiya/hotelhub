6aa0e69dcbc763eb4ad3c8d3cc1160feb4c18d37
Kukadiya arpit <152589706+Arpitkukadiya@users.noreply.github.com>
2025-10-01 15:06:53 +0530

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -13,7 +13,7 @@ $msg= "<h4 style='color:green;'>feedback sent successfully</h4>";
     <div class="container-fluid">
 	<div class="col-sm-4 hov">
 		<img src="logo.png"width="200px"height="50px"/><br><br>
-    <p class="text-justify">A hotel is an establishment that provides paid lodging on a short-term basis. Facilities provided may range from a modest-quality mattress in a small room to large suites with bigger, higher-quality beds, a dresser, a refrigerator and other kitchen facilities, upholstered chairs, a flat screen television, and en-suite bathrooms. Small, lower-priced hotels may offer only the most basic guest services and facilities. Larger, higher-priced hotels may provide additional guest facilities such as a swimming pool, business center</p><br>
+    <p class="text-justify">A hotel is an establishment that provides paid lodging on a short-term basis.Facilities provided may range from a modest-quality mattress in a small room to large suites with bigger, higher-quality beds, a dresser, a refrigerator and other kitchen facilities, upholstered chairs, a flat screen television, and en-suite bathrooms. Small, lower-priced hotels may offer only the most basic guest services and facilities. Larger, higher-priced hotels may provide additional guest facilities such as a swimming pool, business center</p><br>
       <center><a href="about.php" class="btn btn-danger"><b>Read More..</b></a></center><br><br><br>
 
 	</div>&nbsp;&nbsp;
